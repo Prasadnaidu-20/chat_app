9bff0ab42327f21e3136581c7584363b4ac9fe0f
Prasadnaidu-20 <bobbynaidu2005@gmail.com>
2026-03-19 23:23:46 +0530

    ready for deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,8 @@ import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import path from "path";
+
 
 dotenv.config();
 
@@ -14,6 +16,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(cookieParser());
 
@@ -28,6 +31,14 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT,() => {
     console.log("Server is running on port "+ PORT);
